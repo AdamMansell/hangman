934d2cb93df260cd928730138922f3eff5be18f2
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-01 11:53:10 -0700

    Write and PASS game_spec test: create constructor with chosen word selected from an array
    Co-authored-by: Alma Frankenstein <a.q.frankenstein@gmail.com>
    Co-Authored by: Adam Mansell <adammansellhmb@gmail.com>

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,18 +1,19 @@
 require('rspec')
 require('game')
 
-describe('#Game') do
-  describe('#guesses') do
+describe('#Game') do  
+  describe('#guesses') do   # line 2 game.rb: attr method, not explicit method
     it('gives an array of guesses') do
       game = Game.new()
       expect(game.guesses).to(eq([]))
     end
   end
 
-  # describe('#chosen_word') do
-  #   it('returns selected word') do
-      
-  #   end
-  # end
+  describe('#chosen_word') do
+    it('returns selected word') do
+      game = Game.new()
+      expect(game.chosen_word).to(eq('cracking' || 'codes' || 'building' || 'breaking' || 'python' || 'ciphers'))
+    end
+  end
 
 end
