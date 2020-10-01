e0eb5e326f3d6f421aaf4fbf3712254c625391b9
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-01 11:49:57 -0700

    Write and PASS game_spec test: create a game constructor
    Co-authored-by: Alma Frankenstein <a.q.frankenstein@gmail.com>
    Co-Authored by: Adam Mansell <adammansellhmb@gmail.com>

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,74 @@
 class Game
+  attr_reader :chosen_word, :guesses
 
+  def initialize()
+    @possible_words = ['cracking', 'codes', 'building', 'breaking', 'python', 'ciphers']
+    @chosen_word = @possible_words.sample
+    @guesses = []
+  end
+
+  # methods
+  # draw gallows
+  # draw head
+  # draw head, body
+  # draw rarm
+  # draw larm
+  # draw rleg
+  # draw lleg
+
+  # Guess (letter)
+  # Check if letter already in @guesses, and return error message
+  # Check if the  letter is in the@word
+    # Populate the letter in @word
+  # If the letter is not in @word
+    # Add the guess to the @guesses array
+    # call a draw method
+
+# Display word
+
+# Win Method
+
+# Lose Method
+
+# Play Method
+  # Check for wins and loss scenarios
+  # Run guess method
 
   
 end
+
+
+
+
+
+
+
+
+# class Artist
+#   attr_reader :id
+#   attr_accessor :name, :stage_id
+
+#   @@artists = {}
+#   @@total_rows = 0
+
+#   def initialize(name, stage_id, id)
+#     @name = name
+#     @stage_id = stage_id
+#     @id = id || @@total_rows += 1
+#   end
+
+#   def ==(artist_to_compare)
+#     (self.name() == artist_to_compare.name()) && (self.stage_id() == artist_to_compare.stage_id())
+#   end
+
+#   def self.all
+#     @@artists.values
+#   end
+
+#   def save
+#     @@artists[self.id] = Artist.new(self.name, self.stage_id, self.id)
+#   end
+
+#  def self.find(id)
+#     @@artists[id]
+#   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,5 +2,17 @@ require('rspec')
 require('game')
 
 describe('#Game') do
+  describe('#guesses') do
+    it('gives an array of guesses') do
+      game = Game.new()
+      expect(game.guesses).to(eq([]))
+    end
+  end
+
+  # describe('#chosen_word') do
+  #   it('returns selected word') do
+      
+  #   end
+  # end
 
 end
